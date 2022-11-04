2508bff72ec9018fdd8e5ddcce2696ef1af95ad2
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2022-11-04 09:04:21 +0100

    commit

--- main.py
+++ main.py
@@ -38,3 +38,39 @@ Crea algunos productos
 Prueba a mostrar los datos de algun producto y a modificar algun valor, por ejemplo, prueba a modificar el precio de un producto
 
 """""""""""
+
+class alumno(): #Creamos la clase alumno
+
+    def __init__(self, nombre, nota):
+        self.nombre = nombre # Definimos que el atributo nombre, sera el nombre asignado
+        self.nota = nota # Definimos que el atributo nota, sera la nota asignada
+        print
+
+    def constructor(self):
+        alumno = ("Hola soy {} y mi nota es un {}") #Mensaje
+        print(alumno.format(self.nombre, self.nota)) #Usamos FORMAT
+
+    def calificacion(self):
+        if int(self.nota) < 5:
+            print("Ha suspendido")
+        else :
+            print("Ha aprobado")
+        return
+
+alumno1 = alumno("Pedro", "4") #Instancia
+alumno2 = alumno("Cristina", "10")
+alumno3 = alumno("Lucia", "10")
+
+
+
+
+#Llamamos al método
+
+alumno1.constructor()
+alumno1.calificacion()
+
+alumno2.constructor()
+alumno2.calificacion()
+
+alumno3.constructor()
+alumno3.calificacion()
