6031ef10dceb2fc85d0df92a63b9ec74fc279fc8
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2022-11-06 12:27:21 +0100

    commit

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print('\033[1m''\033[96m'"EJERCICIO 1"'\033[96m''\033[0m')
+print('\033[1m''\033[92m'"EJERCICIO 1"'\033[92m''\033[0m')
 import ejercicio1
+
+print("\n\n\n")
+print('\033[1m''\033[95m'"EJERCICIO 2"'\033[95m''\033[0m')
 import ejercicio2
+
+
+print("\n\n\n")
+print('\033[1m''\033[36m'"EJERCICIO 3"'\033[36m''\033[0m')
 import ejercicio3
